6e2a4ba0554c66a95b46d3b043f49ca1dd2bc39d
Alisa Khoroshun <alisakhoroshun@gmail.com>
2024-11-20 02:48:57 +0300

    Добавила функции для связи объектов с сервером.

--- frontend/src/pages/Board.jsx
+++ frontend/src/pages/Board.jsx
@@ -1,303 +1,285 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect, useCallback } from 'react';
 import './Board.scss';
-import { Canvas } from "fabric";
-import { IconButton, Button } from 'blocksin-system';
-import { SquareIcon, TriangleIcon, SlashIcon, TextIcon, StickyNoteIcon, DownloadIcon } from 'sebikostudio-icons';
-import Settings from '../componentsForBoard/Settings.jsx';
-import CanvasSettings from '../componentsForBoard/CanvasSettings.jsx';
-import { addRectangle, addTriangle, addLine, addText, addSticker } from '../componentsForBoard/Shapes.jsx'
-import { saveNameBoard, saveCanvasState } from '../services/boardService.js';
-import axios from "axios";
+import { fabric } from 'fabric';
+import { IconButton } from '@mui/material';
+import { FloppyDiskIcon, DownloadIcon, SquareIcon } from 'sebikostudio-icons';
+import ToolbarComponent from '../componentsForBoard/ToolbarComponent.jsx';
+import { saveNameBoard, saveCanvasState, setupCanvasForObjectTracking, sendObjectToBackend } from '../services/boardService.js';
+import axios from 'axios';
+
 
 function Board() {
 	const canvasRef = useRef(null);
-	const canvasInstanceRef = useRef(null)
-	const [canvas, setCanvas] = useState(null);
+	const canvasInstanceRef = useRef(null);
 	const [boardName, setBoardName] = useState("");
-
+	const [toolbarPosition, setToolbarPosition] = useState("left");
+	const undoStackRef = useRef([]);
+	const clipboardRef = useRef([]);
+	const [canUndo, setCanUndo] = useState(false);
+	const isUndoRedo = useRef(false);
+	const [isCanvasReady, setIsCanvasReady] = useState(false);
 
 	useEffect(() => {
 		if (canvasRef.current) {
-			const initCanvas = new Canvas(canvasRef.current, {
+			const initCanvas = new fabric.Canvas(canvasRef.current, {
 				width: window.innerWidth,
 				height: window.innerHeight,
+				backgroundColor: "#ffffff",
+				selection: true,
 			});
-			initCanvas.backgroundColor = "#ffffff";
-			initCanvas.renderAll();
-			setCanvas(initCanvas);
 			canvasInstanceRef.current = initCanvas;
+			initCanvas.renderAll();
+			setIsCanvasReady(true);
+			setupCanvasForObjectTracking(initCanvas);
+			const initialState = initCanvas.toJSON();
+			undoStackRef.current = [initialState];
+			setCanUndo(false);
+
+			const saveState = () => {
+				if (!isUndoRedo.current) {
+					const currentState = initCanvas.toJSON();
+					undoStackRef.current.push(currentState);
+					if (undoStackRef.current.length > 50) {
+						undoStackRef.current.shift();
+					}
+					setCanUndo(undoStackRef.current.length > 1);
+				}
+			};
+
+			const handleObjectAdded = (e) => {
+				const addedObject = e.target;
+				if (addedObject && !addedObject._fromServer) { // Проверяем, что объект не был загружен с сервера
+					console.log("Добавление объекта:", addedObject);
+					canvasInstanceRef.current.renderAll(); // Обязательно перерисовываем канвас
+					sendObjectToBackend(addedObject);
+				}
+			};
+
+			const handleObjectModified = (e) => {
+				const modifiedObject = e.target;
+				if (modifiedObject) {
+					console.log("Изменение объекта:", modifiedObject);
+					canvasInstanceRef.current.renderAll(); // Обязательно перерисовываем канвас
+					sendObjectToBackend(modifiedObject);
+				}
+			};
+
+
+			initCanvas.on("object:added", handleObjectAdded);
+			initCanvas.on("object:modified", handleObjectModified);
+			setIsCanvasReady(true);
+
+			initCanvas.on('object:added', saveState);
+			initCanvas.on('object:modified', saveState);
+			initCanvas.on('object:removed', saveState);
+
 			return () => {
+				initCanvas.off("object:added", handleObjectAdded);
+				initCanvas.off("object:modified", handleObjectModified);
 				initCanvas.dispose();
 			};
 		}
-
 	}, []);
+
 
 	const handleChange = (e) => {
 		setBoardName(e.target.value);
 	};
 
-
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		try {
 			const response = await saveNameBoard(boardName);
-			console.log('Доска успешно сохранена:', response);
+			console.log('Название доски сохранено:', response);
 		} catch (error) {
-			console.error('Ошибка при сохранении доски:', error);
+			console.error('Ошибка при сохранении названия доски:', error);
 		}
 	};
 
-
 	const handleSaveCanvas = async () => {
 		try {
+			const canvas = canvasInstanceRef.current;
 			await saveCanvasState(canvas);
 			console.log('Состояние доски сохранено.');
 		} catch (error) {
 			console.error('Ошибка при сохранении состояния доски:', error);
 		}
 	};
-   const handleSaveCanvas2 = async () => {
-       try {
-           const response = await axios.get(`http://localhost:8080/message/1`);
-           const canvasData = response.data.text;
-           console.log('Полученные данные:', canvasData);
-           console.log('Тип canvasData:', typeof canvasData);
 
-           const canvasJSON = JSON.parse(canvasData);
-           console.log('canvasJSON:', canvasJSON);
-           console.log('Тип canvasJSON:', typeof canvasJSON);
+	const handleLoadCanvas = async () => {
+		try {
+			const response = await axios.get(`http://localhost:8080/message/24`);
+			const canvasData = response.data.text;
+			const canvasJSON = JSON.parse(canvasData);
+			if (canvasInstanceRef.current) {
+				canvasInstanceRef.current.loadFromJSON(canvasJSON, () => {
+					canvasInstanceRef.current.renderAll();
+					console.log('Доска загружена');
+				});
+			}
+		} catch (error) {
+			console.error('Ошибка при загрузке доски: ', error);
+		}
+	};
 
-           console.log('canvasJSON.objects:', canvasJSON.objects);
+	const handleLoadSingleObject = async () => {
+		try {
+			const response = await axios.get(`http://localhost:8080/message/1732038235684`);
+			const objectData = response.data.text;
+			const parsedObject = JSON.parse(objectData);
 
-           if (canvasInstanceRef.current) {
-               canvasInstanceRef.current.loadFromJSON(canvasJSON, () => {
-                   canvasInstanceRef.current.renderAll();
-                   console.log('Состояние доски загружено.');
-               });
-           } else {
-               console.error('Холст не инициализирован.');
-           }
-       } catch (error) {
-           console.error('Ошибка при загрузке состояния доски:', error);
-       }
-   };
+			console.log("Полученный объект:", parsedObject);
+			if (canvasInstanceRef.current) {
+				let fabricObject;
+				switch (parsedObject.type) {
+						case "rect":
+							fabricObject = new fabric.Rect(parsedObject);
+							break;
+						case "triangle":
+							fabricObject = new fabric.Triangle(parsedObject);
+							break;
+						case "line":
+							fabricObject = new fabric.Line(parsedObject.points, parsedObject);
+							break;
+						case "textbox":
+						case "text":
+							fabricObject = new fabric.Textbox(parsedObject.text, parsedObject);
+							break;
+						default:
+							console.error("Неизвестный тип объекта:", parsedObject.type);
+							return;
+				}
+
+				fabricObject.id = parsedObject.id;
+				fabricObject._fromServer = true;
+				canvasInstanceRef.current.add(fabricObject);
+				canvasInstanceRef.current.renderAll();
+				console.log("Объект добавлен на канвас.");
+			}
+		} catch (error) {
+			console.error("Ошибка при загрузке объекта с сервера:", error);
+		}
+	};
+
+
+
+	const handleUndo = useCallback(() => {
+		const canvas = canvasInstanceRef.current;
+		if (undoStackRef.current.length > 1 && canvas) {
+			isUndoRedo.current = true;
+			undoStackRef.current.pop();
+			const previousState = undoStackRef.current[undoStackRef.current.length - 1];
+			canvas.loadFromJSON(previousState, () => {
+				canvas.renderAll();
+				isUndoRedo.current = false;
+				setCanUndo(undoStackRef.current.length > 1);
+			});
+		}
+	}, []);
+
+	const handleCopy = useCallback(() => {
+		const canvas = canvasInstanceRef.current;
+		const activeObjects = canvas.getActiveObjects();
+		Promise.all(activeObjects.map(obj => {
+			return new Promise((resolve, reject) => {
+				obj.clone(clonedObj => {
+					if (clonedObj) {
+						resolve(clonedObj);
+					}
+				});
+			});
+		}))
+		.then(clonedObjects => {
+			clipboardRef.current = clonedObjects.filter(obj => obj !== undefined);
+		})
+	}, []);
+
+	const handlePaste = useCallback(() => {
+		const canvas = canvasInstanceRef.current;
+		const clipboard = clipboardRef.current;
+		const validClipboard = clipboard.filter(obj => obj !== undefined && typeof obj.clone === 'function');
+		Promise.all(validClipboard.map(obj => {
+			return new Promise((resolve, reject) => {
+				obj.clone(clonedObj => {
+					if (clonedObj) {
+						resolve(clonedObj);
+					}
+				});
+			});
+		}))
+		.then(pastedObjects => {
+			const validPastedObjects = pastedObjects.filter(obj => obj !== undefined);
+			validPastedObjects.forEach(obj => {
+				obj.set({
+					left: obj.left + 10,
+					top: obj.top + 10,
+					evented: true,
+				});
+				canvas.add(obj);
+				canvas.setActiveObject(obj);
+			});
+			canvas.renderAll();
+		})
+	}, []);
+
+	useEffect(() => {
+		const handleKeyDown = (e) => {
+			if (e.ctrlKey && e.key === 'z') {
+				e.preventDefault();
+				handleUndo();
+			}
+			if (e.ctrlKey && e.key === 'c') {
+				e.preventDefault();
+				handleCopy();
+			}
+			if (e.ctrlKey && e.key === 'v') {
+				e.preventDefault();
+				handlePaste();
+			}
+		};
+		window.addEventListener('keydown', handleKeyDown);
+		return () => {
+			window.removeEventListener('keydown', handleKeyDown);
+		};
+	}, [handleUndo, handleCopy, handlePaste]);
 
 	return (
 		<div className='board'>
 			<canvas id='canvas' ref={canvasRef} />
+			<div className={`toolbar-wrapper ${toolbarPosition}`}>
+				<ToolbarComponent
+					canvas={canvasInstanceRef.current}
+					position={toolbarPosition}
+					onChangePosition={setToolbarPosition}
+				/>
+			</div>
+
 			<div className='settings-wrapper darkmode'>
-				<Settings canvas={canvas} />
-				<CanvasSettings canvas={canvas} />
-				<IconButton onClick={() => addRectangle(canvas)} variant="ghost" size="medium">
+				<form className='name-of-board' onSubmit={handleSubmit}>
+					<label>
+						Название доски:
+						<input
+							type="text"
+							value={boardName}
+							onChange={handleChange}
+							placeholder="Доска №1"
+						/>
+					</label>
+					<input type="submit" value="Сохранить" />
+				</form>
+
+				<IconButton onClick={handleSaveCanvas} variant="ghost" size="medium">
+					<FloppyDiskIcon />
+				</IconButton>
+				<IconButton onClick={handleLoadCanvas} variant="ghost" size="medium">
+					<DownloadIcon />
+				</IconButton>
+				<IconButton onClick={handleLoadSingleObject} variant="ghost" size="medium">
 					<SquareIcon />
 				</IconButton>
-				<IconButton onClick={() => addTriangle(canvas)} variant="ghost" size="medium">
-					<TriangleIcon />
-				</IconButton>
-				<IconButton onClick={() => addLine(canvas)} variant="ghost" size="medium">
-					<SlashIcon />
-				</IconButton>
-                <IconButton onClick={() => addText(canvas)} variant="ghost" size="medium">
-                    <TextIcon />
-                </IconButton>
-                <IconButton onClick={() => addSticker(canvas)} variant="ghost" size="medium">
-                    <StickyNoteIcon />
-                </IconButton>
-
-
-                <Button onClick={handleSaveCanvas}>
-                    Сохранить доску
-                </Button>
-                 <IconButton onClick={handleSaveCanvas2} variant="ghost" size="medium">
-                     <DownloadIcon />
-                  </IconButton>
-
 			</div>
 		</div>
 	);
 }
 
 export default Board;
-
-
-import {over} from 'stompjs';
-import SockJS from 'sockjs-client';
-
-var stompClient =null;
-const ChatRoom = () => {
-    const [privateChats, setPrivateChats] = useState(new Map());
-    const [publicChats, setPublicChats] = useState([]);
-    const [tab,setTab] =useState("CHATROOM");
-    const [userData, setUserData] = useState({
-        username: '',
-        receivername: '',
-        connected: false,
-        message: ''
-      });
-    useEffect(() => {
-      console.log(userData);
-    }, [userData]);
-
-    const connect =()=>{
-        let Sock = new SockJS('http://localhost:8080/ws');
-        stompClient = over(Sock);
-        stompClient.connect({},onConnected, onError);
-    }
-
-    const onConnected = () => {
-        setUserData({...userData,"connected": true});
-        stompClient.subscribe('/chatroom/public', onMessageReceived);
-        stompClient.subscribe('/user/'+userData.username+'/private', onPrivateMessage);
-        userJoin();
-    }
-
-    const userJoin=()=>{
-          var chatMessage = {
-            senderName: userData.username,
-            status:"JOIN"
-          };
-          stompClient.send("/app/message", {}, JSON.stringify(chatMessage));
-    }
-
-    const onMessageReceived = (payload)=>{
-        var payloadData = JSON.parse(payload.body);
-        switch(payloadData.status){
-            case "JOIN":
-                if(!privateChats.get(payloadData.senderName)){
-                    privateChats.set(payloadData.senderName,[]);
-                    setPrivateChats(new Map(privateChats));
-                }
-                break;
-            case "MESSAGE":
-                publicChats.push(payloadData);
-                setPublicChats([...publicChats]);
-                break;
-        }
-    }
-
-    const onPrivateMessage = (payload)=>{
-        console.log(payload);
-        var payloadData = JSON.parse(payload.body);
-        if(privateChats.get(payloadData.senderName)){
-            privateChats.get(payloadData.senderName).push(payloadData);
-            setPrivateChats(new Map(privateChats));
-        }else{
-            let list =[];
-            list.push(payloadData);
-            privateChats.set(payloadData.senderName,list);
-            setPrivateChats(new Map(privateChats));
-        }
-    }
-
-    const onError = (err) => {
-        console.log(err);
-
-    }
-
-    const handleMessage =(event)=>{
-        const {value}=event.target;
-        setUserData({...userData,"message": value});
-    }
-    const sendValue=()=>{
-            if (stompClient) {
-              var chatMessage = {
-                senderName: userData.username,
-                message: userData.message,
-                status:"MESSAGE"
-              };
-              console.log(chatMessage);
-              stompClient.send("/app/message", {}, JSON.stringify(chatMessage));
-              setUserData({...userData,"message": ""});
-            }
-    }
-
-    const sendPrivateValue=()=>{
-        if (stompClient) {
-          var chatMessage = {
-            senderName: userData.username,
-            receiverName:tab,
-            message: userData.message,
-            status:"MESSAGE"
-          };
-
-          if(userData.username !== tab){
-            privateChats.get(tab).push(chatMessage);
-            setPrivateChats(new Map(privateChats));
-          }
-          stompClient.send("/app/private-message", {}, JSON.stringify(chatMessage));
-          setUserData({...userData,"message": ""});
-        }
-    }
-
-    const handleUsername=(event)=>{
-        const {value}=event.target;
-        setUserData({...userData,"username": value});
-    }
-
-    const registerUser=()=>{
-        connect();
-    }
-    return (
-    <div className="container">
-        {userData.connected?
-        <div className="chat-box">
-            <div className="member-list">
-                <ul>
-                    <li onClick={()=>{setTab("CHATROOM")}} className={`member ${tab==="CHATROOM" && "active"}`}>Chatroom</li>
-                    {[...privateChats.keys()].map((name,index)=>(
-                        <li onClick={()=>{setTab(name)}} className={`member ${tab===name && "active"}`} key={index}>{name}</li>
-                    ))}
-                </ul>
-            </div>
-            {tab==="CHATROOM" && <div className="chat-content">
-                <ul className="chat-messages">
-                    {publicChats.map((chat,index)=>(
-                        <li className={`message ${chat.senderName === userData.username && "self"}`} key={index}>
-                            {chat.senderName !== userData.username && <div className="avatar">{chat.senderName}</div>}
-                            <div className="message-data">{chat.message}</div>
-                            {chat.senderName === userData.username && <div className="avatar self">{chat.senderName}</div>}
-                        </li>
-                    ))}
-                </ul>
-
-                <div className="send-message">
-                    <input type="text" className="input-message" placeholder="enter the message" value={userData.message} onChange={handleMessage} />
-                    <button type="button" className="send-button" onClick={sendValue}>send</button>
-                </div>
-            </div>}
-            {tab!=="CHATROOM" && <div className="chat-content">
-                <ul className="chat-messages">
-                    {[...privateChats.get(tab)].map((chat,index)=>(
-                        <li className={`message ${chat.senderName === userData.username && "self"}`} key={index}>
-                            {chat.senderName !== userData.username && <div className="avatar">{chat.senderName}</div>}
-                            <div className="message-data">{chat.message}</div>
-                            {chat.senderName === userData.username && <div className="avatar self">{chat.senderName}</div>}
-                        </li>
-                    ))}
-                </ul>
-
-                <div className="send-message">
-                    <input type="text" className="input-message" placeholder="enter the message" value={userData.message} onChange={handleMessage} />
-                    <button type="button" className="send-button" onClick={sendPrivateValue}>send</button>
-                </div>
-            </div>}
-        </div>
-        :
-        <div className="register">
-            <input
-                id="user-name"
-                placeholder="Enter your name"
-                name="userName"
-                value={userData.username}
-                onChange={handleUsername}
-                margin="normal"
-              />
-              <button type="button" onClick={registerUser}>
-                    connect
-              </button>
-        </div>}
-    </div>
-    )
-}
-
--- frontend/src/services/boardService.js
+++ frontend/src/services/boardService.js
@@ -12,23 +12,47 @@ export const saveNameBoard = async (boardName) => {
 };
 
 export const saveCanvasState = async(canvas) => {
-    const canvasData = JSON.stringify(canvas.toJSON());
-    console.log((canvasData));
-    await axios.post(`${API_URL}/message`, { text: canvasData });
-    console.log((canvasData));
+	const canvasData = JSON.stringify(canvas.toObject());
+	await axios.post(`${API_URL}/message`, { text: canvasData });
+};
 
+export const sendObjectToBackend = async (object) => {
+	try {
+		const objectData = object.toObject(); // Преобразуем объект в формат JSON
+		const payload = {
+			id: objectData.id, // Передаём id объекта
+			text: JSON.stringify(objectData) // Передаём объект в текстовом формате
+		};
+
+		await axios.post(`${API_URL}/message`, payload); // Отправляем данные на сервер
+		console.log("Объект успешно отправлен на сервер:", payload);
+	} catch (error) {
+		console.error("Ошибка при отправке объекта на сервер:", error);
+	}
 };
-export const  saveCanvasState23 = async (canvas) => {
-  try {
-    const response = await axios.get(`${API_URL}/message/10`);
-    const canvasData = response.data.text; // Предполагаем, что данные находятся в поле 'text'
-    // Парсим строку JSON обратно в объект
-    const canvasJSON = JSON.parse(canvasData);
-    console.log((canvasData))
-    // Загружаем состояние холста
-    canvas.loadFromJSON(canvasJSON, () => {      // Рендерим холст после загрузки
-    canvas.renderAll();    });
-    console.log((canvasData))
-  } catch (error) {
-    console.error('Ошибка при загрузке состояния холста:', error);  }
+
+export const putObjectToBackend = async (object) => {
+	try {
+		const objectData = object.toObject(); // Преобразуем объект в формат JSON
+		const id = objectData.id
+		console.log(`${API_URL}/message/${id}`, { text: JSON.stringify(objectData) })
+
+		await axios.put(`${API_URL}/message/${id}`, { text: JSON.stringify(objectData) }); // Отправляем данные на сервер
+		console.log("Объект успешно отправлен на сервер:", JSON.stringify(objectData)	);
+	} catch (error) {
+		console.error("Ошибка при отправке объекта на сервер:", JSON.stringify(objectData));
+	}
 };
+
+
+export const setupCanvasForObjectTracking = (canvas) => {
+	if (!canvas) return;
+	canvas.on("object:added", (e) => {
+		const addedObject = e.target;
+		if (addedObject) {
+			sendObjectToBackend(addedObject);
+		}
+	});
+	console.log("Настройка отслеживания объектов завершена");
+};
+
