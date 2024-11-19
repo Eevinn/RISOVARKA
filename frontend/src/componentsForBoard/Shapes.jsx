a864965eef812b6e338da7fcbc93068b861e837c
Alisa Khoroshun <alisakhoroshun@gmail.com>
2024-11-20 02:48:57 +0300

    Добавила функции для связи объектов с сервером.

--- frontend/src/componentsForBoard/Shapes.jsx
+++ frontend/src/componentsForBoard/Shapes.jsx
@@ -1,7 +1,6 @@
-import { Rect, Triangle, Line, Textbox, Group } from "fabric";
+import { fabric } from "fabric";
 
-
-export const assignIdToShape = (shape, prefix) => {
+export const assignIdToShape = (shape) => {
 	shape.id = `${new Date().getTime()}`;
 	shape.toObject = (function(toObject) {
 		return function() {
@@ -15,17 +14,19 @@ export const assignIdToShape = (shape, prefix) => {
 };
 
 
+
 export const addRectangle = (canvas) => {
 	if (canvas) {
-		const rect = new Rect({
+		const rect = new fabric.Rect({
 			top: 100,
 			left: 200,
 			width: 100,
 			height: 60,
-			fill: "#42D884",
+			fill: "#FF0000",
 		});
 		assignIdToShape(rect);
 		canvas.add(rect);
+		canvas.renderAll();
 		console.log("прямоугольник", JSON.stringify(rect.toObject()));
 	}
 };
@@ -33,15 +34,16 @@ export const addRectangle = (canvas) => {
 
 export const addTriangle = (canvas) => {
 	if (canvas) {
-		const triangle = new Triangle({
+		const triangle = new fabric.Triangle({
 			top: 200,
 			left: 100,
 			width: 100,
 			height: 100,
-			fill: "#42D884",
+			fill: "#FF0000",
 		});
 		assignIdToShape(triangle);
 		canvas.add(triangle);
+		canvas.renderAll();
 		console.log("треугольник", JSON.stringify(triangle.toObject()));
 	}
 };
@@ -49,90 +51,43 @@ export const addTriangle = (canvas) => {
 
 export const addLine = (canvas) => {
 	if (canvas) {
-		const line = new Line([100, 100, 200, 200], {
-			stroke: "#42D884",
+		const line = new fabric.Line([100, 100, 200, 200], {
+			stroke: "#FF0000",
 			strokeWidth: 4,
 		});
 		assignIdToShape(line);
 		canvas.add(line);
+		canvas.renderAll();
 		console.log("линия", JSON.stringify(line.toObject()));
 	}
 };
 
 
 export const addText = (canvas) => {
-    if (canvas) {
-        const text = new Textbox("", {
-            top: 300,
-            left: 100,
-            width: 200,
-            height: 50,
-            fontSize: 20,
-            fill: "#42D884",
-            textAlign: "center",
-            editable: true,
-        });
-        assignIdToShape(text);
-        canvas.add(text);
-        canvas.setActiveObject(text);
-    }
+	if (canvas) {
+		const text = new fabric.Textbox("txt", {
+			left: 100,
+			top: 100,
+			width: 200,
+			height: 50,
+			fontSize: 24,
+			fill: "black",
+			editable: true,
+			textAlign: "center",
+		});
+		assignIdToShape(text);
+		canvas.add(text);
+		canvas.renderAll();
+		canvas.setActiveObject(text);
+	}
+};
+
+export const deleteSelectedObject = (canvas) => {
+	const activeObject = canvas.getActiveObject();
+	if (activeObject) {
+		canvas.remove(activeObject);
+		canvas.renderAll();
+	}
 };
 
 
-const sizeOfSticker = (text, rect) => {
-    if (text.width > rect.width - 20) {
-        rect.set({ width: text.width + 20 });
-    }
-    if (text.height > rect.height - 20) {
-        rect.set({ height: text.width + 20 });
-    }
-
-    rect.setCoords();
-};
-
-export const addSticker = (canvas) => {
-    if (canvas) {
-        const stickerBackground = new Rect ({
-            width: 200,
-            height: 150,
-            fill: "#42D884",
-            rx: 20,
-            ry: 20,
-        });
-
-        const stickerText = new Textbox ("txt", {
-           width: 180,
-           fontSize: 24,
-           left: 10,
-           top: 10,
-           fill: "black",
-           textAlign: "center",
-           editable: true,
-        });
-
-        const sticker = new Group ([stickerBackground, stickerText], {
-           left: 100,
-           top: 100,
-        });
-
-        canvas.add(sticker);
-        canvas.setActiveObject(sticker);
-        console.log("cnbrth", JSON.stringify(sticker.toObject(), null, 2));
-
-        sticker.on('mousedblclick', () => {
-            stickerText.enterEditing();
-            canvas.setActiveObject(stickerText);
-            stickerText.selectAll();
-        });
-
-        stickerText.on('editing:exited', () => {
-            sizeOfSticker(stickerText, stickerBackground);
-            canvas.renderAll();
-        });
-
-        stickerText.on('changed', () => {
-            sizeOfSticker(stickerText, stickerBackground);
-            canvas.renderAll();
-        });
-    }
-};
