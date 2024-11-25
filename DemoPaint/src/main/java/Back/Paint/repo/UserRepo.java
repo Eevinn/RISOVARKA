d5b0924e5ea3fe3606eacb4fcfe107e8288dc318
Alisa_Khrn <alisakhoroshun@gmail.com>
2024-11-25 14:05:11 +0300

    Добавлена авторизация

--- DemoPaint/src/main/java/Back/Paint/repo/UserRepo.java
+++
@@ -1,7 +0,0 @@
-package Back.Paint.repo;
-import Back.Paint.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, Long> {
-
-}
